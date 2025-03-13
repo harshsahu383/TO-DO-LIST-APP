3595fd717e8992df168f0d46abf0bab8b479743f
harshsahu383 <harshsahu1917@gmail.com>
2025-03-14 00:36:18 +0530

    fixes some bugs in delete function

--- dist/JS/Task.js
+++ dist/JS/Task.js
@@ -1,3 +1,7 @@
+const customAlert = document.getElementById('customAlert');
+const alertMessage = document.getElementById('alertMessage');
+const closeAlertBtn = document.getElementById('closeAlertBtn');
+const alertIcon = document.getElementById('alertIcon');
 const addTaskModal = document.getElementById('addTaskModal');
 const overlay = document.getElementById('overlay');
 const addNewTaskBtn = document.getElementById('addNewTaskBtn');
@@ -12,55 +16,16 @@ const closeDeleteModalBtn = document.getElementById('closeDeleteModalBtn');
 const closeDeleteFormBtn = document.getElementById('closeDeleteFormBtn');
 const deleteTaskBtn = document.getElementById('deleteTaskBtn');
 const deleteTaskModalBtn = document.getElementById('deleteTaskModalBtn');
-
-addNewTaskBtn.addEventListener("click", function(e){
-    e.preventDefault();
-    addTaskModal.classList.add('open');
-    overlay.classList.add('active');
-
-});
-closeModalBtn.addEventListener("click", function(e){
-    addTaskModal.classList.remove('open');
-    addTaskModal.classList.remove('active');
-});
-overlay.addEventListener("click", (e) => {
-    addTaskModal.classList.remove('open');
-    overlay.classList.remove('active');
-});
-closeAddFormBtn.addEventListener('click', () => {
-    addTaskModal.classList.remove('open');
-    overlay.classList.remove('active');
-})
-// addTaskBtn.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     alert("Task Added successFully");
-// })
-taskForm.addEventListener('submit', (e) => {
-e.preventDefault();
-
-const taskTitle = document.getElementById('task-title').value.trim();
-const taskDescription= document.getElementById('task-description').value.trim();
-const dueDate= document.getElementById('due-date').value.trim();
-const priority= document.getElementById('priority').value;
-
-if(taskTitle && taskDescription && dueDate && priority){
-    showAlert("Task Added Succesfully",'success');
-
-    taskForm.reset();
-    addTaskModal.classList.remove('open');
-    overlay.classList.remove('active');
+const taskListBtn =document.getElementById('taskListBtn');
+const taskListModal = document.getElementById('taskListModal');
+const closeTaskListBtn = document.getElementById('closeTaskListBtn');
+const tasksContainer = document.getElementById('tasksContainer');
+// functions
+function saveTaskToLocalStorage(task){
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    tasks.push(task);
+    localStorage.setItem('tasks',JSON.stringify(tasks));
 }
-else{
-    showAlert("Fill all the columns First", "error")
-}
-
-}
-);
-const customAlert = document.getElementById('customAlert');
-const alertMessage = document.getElementById('alertMessage');
-const closeAlertBtn = document.getElementById('closeAlertBtn');
-const alertIcon = document.getElementById('alertIcon');
-
 function showAlert (message,type = "success"){
     alertMessage.textContent = message;
     if(type==='success'){
@@ -81,9 +46,59 @@ function showAlert (message,type = "success"){
     }, 3000);
 
 }
+function fetchTasks (){
+    return JSON.parse(localStorage.getItem('tasks')) || [];
+}
+function displayTasks () {
+    const tasks = fetchTasks();
+    tasksContainer.innerHTML='';
+    if(tasks.length===0){
+        overlay.classList.remove('active');
+        taskListModal.classList.remove('open');
+        return showAlert("No tasks Are Found",'error');
+    }
+    tasks.forEach(task => {
+        const taskElement = document.createElement('div');
+        taskElement.classList = "bg-gray rounded-lg p-4";
+        taskElement.innerHTML = `
+        <h2 class="text-lg font-semibold text-gray-800">Task ID</h2>
+        <h3 class="text-1xl text-gray-800">${task.id}</h3>
+        <p class="text-gray-600 "> <span class=" text-black">Task Title : </span>  ${task.title}</p> 
+        <p class="text-gray-600 ">  <span class=" text-black">Task Description : </span> ${task.description}</p> 
+        <p class="text-gray-600 "> <span class=" text-black">Task DueDate :</span>  ${task.dueDate}</p> 
+        <p class="text-gray-600 "><span class=" text-black">Task Priority :</span> ${task.priority}</p> 
+        <p class="text-gray-600 "><span class=" text-black">Date  :</span> ${task.date}</p> 
+        `;
+        tasksContainer.append(taskElement);
+    });
+}
+
+// end functions
+
+addNewTaskBtn.addEventListener("click", function(e){
+    e.preventDefault();
+    addTaskModal.classList.add('open');
+    overlay.classList.add('active');
+
+});
+closeModalBtn.addEventListener("click", function(e){
+    addTaskModal.classList.remove('open');
+    addTaskModal.classList.remove('active');
+});
+overlay.addEventListener("click", (e) => {
+    addTaskModal.classList.remove('open');
+    taskListModal.classList.remove('open');
+    deleteTaskModal.classList.remove('open');
+    overlay.classList.remove('active');
+});
+closeAddFormBtn.addEventListener('click', () => {
+    addTaskModal.classList.remove('open');
+    overlay.classList.remove('active');
+});
+ 
 closeAlertBtn.addEventListener('click', () => {
     customAlert.classList.add('hidden');
-})
+});
 
 markAllAsDone.addEventListener('click', () => {
     showAlert("All Marked AS Done",'success')
@@ -104,11 +119,7 @@ closeDeleteModalBtn.addEventListener('click',(e) => {
     deleteTaskModal.classList.remove('open');
     overlay.classList.remove('active');
 });
-overlay.addEventListener('click',(e) => {
-    e.preventDefault();
-    deleteTaskModal.classList.remove('open');
-    overlay.classList.remove('active');
-});
+
 deletetaskForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const taskId = document.getElementById('task-id').value;
@@ -118,8 +129,8 @@ deletetaskForm.addEventListener('submit', (e) => {
     if(taskId){
         showAlert(`${taskId} is Deleted Succesfully`,'success');
         deletetaskForm.reset();
-        deleteTaskModal.remove('open');
-        overlay.remove('active');
+        deleteTaskModal.classList.remove('open');
+        overlay.classList.remove('active');
     }
     else {
         showAlert(`${taskId} is incorrect`,'error');
@@ -130,3 +141,50 @@ deletetaskForm.addEventListener('submit', (e) => {
         
     }
 });
+/// add task form functioning
+
+taskForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const taskTitle = document.getElementById('task-title').value.trim();
+    const taskDescription= document.getElementById('task-description').value.trim();
+    const dueDate= document.getElementById('due-date').value.trim();
+    const priority= document.getElementById('priority').value;
+
+    
+    if(taskTitle && taskDescription && dueDate && priority){
+        const task = {
+            id : Date.now(),
+            title : taskTitle,
+            description : taskDescription,
+            dueDate : dueDate,
+            priority : priority,
+            date : new Date().toLocaleDateString('en-GB'),
+        };
+        saveTaskToLocalStorage(task);
+        showAlert("Task Added Successfully",'success')
+        taskForm.reset();
+        addTaskModal.classList.remove('open');
+        overlay.classList.remove('active');
+        displayTasks();
+        
+    }
+    else{
+        showAlert("Fill all the columns First", "error")
+    } 
+    }
+    );
+   
+    taskListBtn.addEventListener('click', (e)=>{
+        e.preventDefault();
+        taskListModal.classList.add('open');
+        overlay.classList.add('active')
+        displayTasks();
+        
+    });
+    closeTaskListBtn.addEventListener('click',(e)=>{
+        taskListModal.classList.remove('open');
+        overlay.classList.remove('active')
+    });
+    
+    
